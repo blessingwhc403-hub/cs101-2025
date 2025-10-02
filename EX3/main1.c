1a346fed274b1981788a35605a711fa5a9e1eb26
blessingwhc403-hub <blessingwhc403@gmail.com>
2025-10-02 15:27:18 +0800

    Update main1.c

--- EX3/main1.c
+++ EX3/main1.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 
-void print_sp(int i, int n) {
+void print_sb(int i, int n) {
     for (int j = 0; j < n - i; j++) {
         printf(" ");
     }
 }
 
 
-void print_num(int n) {
+void print_cum(int n) {
     for (int j = 0; j < n; j++) {
         printf("%d ", n);
     }
 }
 
 int main() {
-    int rows = 6;
-    for (int i = 1; i <= rows; i++) {
-        print_sp(i, rows);   
-        print_num(i);        
+    int qqq = 6;
+    for (int i = 1; i <= qqq; i++) {
+        print_sb(i, qqq);   
+        print_cum(i);        
         printf("\n");        
     }
     return 0;
