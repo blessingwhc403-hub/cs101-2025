8e8ae07b2c89566948eeaca71f71b60998734558
blessingwhc403-hub <blessingwhc403@gmail.com>
2025-10-02 14:16:00 +0800

    Create main1.c

---
+++ EX3/main1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+void print_sp(int i, int n) {
+    for (int j = 0; j < n - i; j++) {
+        printf(" ");
+    }
+}
+
+
+void print_num(int n) {
+    for (int j = 0; j < n; j++) {
+        printf("%d ", n);
+    }
+}
+
+int main() {
+    int rows = 6;
+    for (int i = 1; i <= rows; i++) {
+        print_sp(i, rows);   
+        print_num(i);        
+        printf("\n");        
+    }
+    return 0;
+}
